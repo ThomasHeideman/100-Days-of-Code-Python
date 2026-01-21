8db0ff9da153bc22b818310de21f5d6a486d4ee8
ThomasHeideman <47209974+ThomasHeideman@users.noreply.github.com>
2026-01-21 21:45:48 +0100

    * included docstrings * refactored set_difficulty() to return global variables

--- Day 12/Number Guessing Project/main.py
+++ Day 12/Number Guessing Project/main.py
@@ -1,16 +1,27 @@
-import random
+from random import randint
 from art import logo
 continue_playing = True
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
 
 def set_difficulty():
+    """
+        Prompts the user to select a difficulty level and determines the starting number of attempts.
+        :return: The value of either EASY_LEVEL_TURNS or HARD_LEVEL_TURNS based on user input.
+    """
     print("welcome to the Number Guessing game.\nI'm thinking of a number between 1 and 100.")
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
     if difficulty == 'hard':
-        return 5
+        return EASY_LEVEL_TURNS
     else:
-        return 10
+        return HARD_LEVEL_TURNS
 
 def guess_function(guess_input,secret):
+    """
+        Runs the main game loop where the user makes guesses until they win or run out of turns.
+        :param guess_input: The initial number of attempts based on difficulty.
+        :param secret: The random target number the user needs to guess.
+        """
     while guess_input > 0:
         guess = int(input(f'You have {guess_input} attempts remaining to guess the number.\nMake a guess: '))
         guess_input -= 1
@@ -25,9 +36,12 @@ def guess_function(guess_input,secret):
 
 
 def play_game():
+    """
+        Initializes a single round of the Number Guessing game, including the logo and secret number generation.
+        """
     print(logo)
 
-    secret_number = random.randint(1, 100)
+    secret_number = randint(1, 100)
     num_guesses = set_difficulty()
 
     guess_function(num_guesses,secret_number)
