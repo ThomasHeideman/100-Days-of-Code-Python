880243281d16fdd4741fe77d7cd92dfb4897dae2
ThomasHeideman <47209974+ThomasHeideman@users.noreply.github.com>
2026-01-25 21:27:56 +0100

    feat: integrate OpenTDB API for live questions and unescape HTML entities

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,11 +1,20 @@
+from requests import get
+from html import unescape
+# from data import question_data
 from question_model import Question
-from data import question_data
 from quiz_brain import QuizBrain
+
+
+url = "https://opentdb.com/api.php?amount=20&difficulty=easy&type=boolean"
+response = get(url)
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]
 question_bank = []
 
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = unescape(question["question"])
+    question_answer = question["correct_answer"]
     new_question = Question(question_text,question_answer)
     question_bank.append(new_question)
 new_quiz = QuizBrain(question_bank)
