76277cab3482939d7b60c974744a3fa234cdcc16
ThomasHeideman <47209974+ThomasHeideman@users.noreply.github.com>
2026-02-03 16:10:35 +0100

    refactor: adjusted mail merge project to return .docx files

--- Day_024/Mail_Merge_project/main.py
+++ Day_024/Mail_Merge_project/main.py
@@ -5,7 +5,7 @@ with open('./Input/Names/invited_names.txt') as names:
     for line in names:
             name = line.strip("\n")
             x = txt.replace("[name]", name)
-            with open(f"./Output/ReadyToSend/Letter_To_{name.replace(' ', '_')}", mode="w") as new_file:
+            with open(f"./Output/ReadyToSend/Letter_To_{name.replace(' ', '_')}.docx", mode="w") as new_file:
                 new_file.write(x)
 
 
